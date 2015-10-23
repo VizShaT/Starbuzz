37d05053d3c9d244f6edbfa9279b4ae08645e3dd
Kikit <kikit@users.noreply.github.com>
2015-10-24 00:36:01 +0530

    DrinkActivity added

--- app/src/main/java/kikit/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/kikit/starbuzz/DrinkCategoryActivity.java
@@ -1,7 +1,9 @@
 package kikit.starbuzz;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -17,5 +19,11 @@ public class DrinkCategoryActivity extends ListActivity {
                 Drink.drinks
         );
         listDrinks.setAdapter(listAdapter);
+    }
+    @Override
+    public void onListItemClick(ListView listView, View itemView, int position, long id){
+        Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
+        intent.putExtra(DrinkActivity.EXTRA_DRINKNO, (int) id);
+        startActivity(intent);
     }
 }
